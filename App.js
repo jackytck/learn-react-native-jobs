18e7ae9d0e8a7b38f8c63bbf198950b612345a7a
Jacky Tang <jackytck@gmail.com>
2017-09-15 00:56:29 +0800

    :construction: add navigation

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import {
+  TabNavigator,
+  StackNavigator
+} from 'react-navigation'
+
+import AuthScreen from './screens/AuthScreen'
+import WelcomeScreen from './screens/WelcomeScreen'
+import MapScreen from './screens/MapScreen'
+import DeckScreen from './screens/DeckScreen'
+import SettingsScreen from './screens/SettingsScreen'
+import ReviewScreen from './screens/ReviewScreen'
 
 export default class App extends React.Component {
   render () {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        })
+      }
+    },
+    {
+      tabBarPosition: 'bottom',
+      swipeEnabled: false,
+      lazyLoad: true,
+      animationEnabled: false
+    })
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <MainNavigator />
       </View>
     )
   }
@@ -15,7 +49,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center'
   }
 })
--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -5,6 +5,11 @@ import {
 } from 'react-native'
 
 class ReviewScreen extends Component {
+  static navigationOptions = {
+    title: 'Review Jobs',
+    headerRight: <Text>Go Right</Text>
+  }
+
   render () {
     return (
       <View>
