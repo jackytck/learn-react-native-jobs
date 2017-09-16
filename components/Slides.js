e968f2c9f30f1be41ad0ca418660b2b97abed508
Jacky Tang <jackytck@gmail.com>
2017-09-17 00:01:45 +0800

    :sparkles: add components: Slides

---
+++ components/Slides.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import {
+  View,
+  Text,
+  ScrollView,
+  Dimensions
+} from 'react-native'
+
+const SCREEN_WIDTH = Dimensions.get('window').width
+
+class Slides extends Component {
+  renderSlides () {
+    return this.props.data.map(slide => {
+      return (
+        <View
+          key={slide.text}
+          style={[styles.slideStyle, { backgroundColor: slide.color }]}
+        >
+          <Text style={styles.textStyle}>{slide.text}</Text>
+        </View>
+      )
+    })
+  }
+
+  render () {
+    return (
+      <ScrollView
+        horizontal
+        pagingEnabled
+        style={{ flex: 1 }}
+      >
+        {this.renderSlides()}
+      </ScrollView>
+    )
+  }
+}
+
+Slides.propTypes = {
+  data: PropTypes.array
+}
+
+const styles = {
+  slideStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: SCREEN_WIDTH
+  },
+  textStyle: {
+    fontSize: 30,
+    color: 'white'
+  }
+}
+
+export default Slides
