15545bbdc38ff37af725e1a1efdde56f72b06a91
Jacky Tang <jackytck@gmail.com>
2017-09-27 00:06:39 +0800

    :sparkles: dispatch fetchJobs action in MapScreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import {
   View,
   ActivityIndicator
 } from 'react-native'
 import { MapView } from 'expo'
+import { connect } from 'react-redux'
+import {
+  Button
+} from 'react-native-elements'
+
+import * as actions from '../actions'
 
 class MapScreen extends Component {
   state = {
@@ -24,6 +31,10 @@ class MapScreen extends Component {
     this.setState({ region })
   }
 
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region)
+  }
+
   render () {
     if (!this.state.mapLoaded) {
       return (
@@ -40,9 +51,31 @@ class MapScreen extends Component {
           style={{ flex: 1 }}
           onRegionChangeComplete={this.onRegionChangeComplete}
         />
+        <View style={styles.buttonContainer}>
+          <Button
+            large
+            title='Search this area'
+            backgroundColor='#009688'
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     )
   }
 }
 
-export default MapScreen
+MapScreen.propTypes = {
+  fetchJobs: PropTypes.func
+}
+
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0
+  }
+}
+
+export default connect(null, actions)(MapScreen)
