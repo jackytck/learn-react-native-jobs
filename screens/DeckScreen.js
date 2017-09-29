d15dd8cc98a2fa35b6abfc8d5fe7f5267853fabc
Jacky Tang <jackytck@gmail.com>
2017-09-30 00:19:26 +0800

    :construction: render job card in DeckScreen

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import {
-  View
+  View,
+  Text
 } from 'react-native'
 import { connect } from 'react-redux'
+import { MapView } from 'expo'
+import {
+  Card,
+  Button
+} from 'react-native-elements'
 import Swipe from '../components/Swipe'
 
 class DeckScreen extends Component {
+  renderCard ({ jobtitle, company, formattedRelativeTime, snippet }) {
+    return (
+      <Card title={jobtitle}>
+        <View style={styles.detailWrapper}>
+          <Text>{company}</Text>
+          <Text>{formattedRelativeTime}</Text>
+        </View>
+        <Text>
+          {snippet.replace(/<\/*b>/g, '')}
+        </Text>
+      </Card>
+    )
+  }
+
+  renderNoMoreCards () {
+    return (
+      <Card title='No more jobs' />
+    )
+  }
+
   render () {
     return (
       <View>
         <Swipe
           data={this.props.jobs}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
         />
       </View>
     )
@@ -20,6 +48,14 @@ class DeckScreen extends Component {
 
 DeckScreen.propTypes = {
   jobs: PropTypes.array
+}
+
+const styles = {
+  detailWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10
+  }
 }
 
 function mapStateToProps ({ jobs }) {
