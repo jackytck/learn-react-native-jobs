67319c079ed4ca4ad1c6609c1813e4d3050b80ff
Jacky Tang <jackytck@gmail.com>
2017-10-03 00:17:35 +0800

    :construction: render liked jobs in ReviewScreen

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,9 +1,16 @@
+import {
+  Button,
+  Card
+} from 'react-native-elements'
 import React, { Component } from 'react'
 import {
-  View,
-  Text
+  ScrollView,
+  Text,
+  View
 } from 'react-native'
-import { Button } from 'react-native-elements'
+
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
 class ReviewScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -18,18 +25,49 @@ class ReviewScreen extends Component {
     )
   })
 
+  renderLikedJobs () {
+    return this.props.likedJobs.map(job => {
+      return (
+        <Card>
+          <View style={{ height: 200 }}>
+            <View style={styles.detailWrapper}>
+              <Text style={styles.italics}>{job.company}</Text>
+              <Text style={styles.italics}>{job.formattedRelativeTime}</Text>
+            </View>
+          </View>
+        </Card>
+      )
+    })
+  }
+
   render () {
     return (
-      <View>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-      </View>
+      <ScrollView>
+        {this.renderLikedJobs()}
+      </ScrollView>
     )
   }
 }
 
-export default ReviewScreen
+ReviewScreen.propTypes = {
+  likedJobs: PropTypes.array
+}
+
+const styles = {
+  italics: {
+    fontStyle: 'italic'
+  },
+  detailWrapper: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  }
+}
+
+function mapStateToProps (state) {
+  return {
+    likedJobs: state.likedJobs
+  }
+}
+
+export default connect(mapStateToProps)(ReviewScreen)
