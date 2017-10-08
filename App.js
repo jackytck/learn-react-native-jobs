fc3ac839e40950443c3937e883080319e61149c1
Jacky Tang <jackytck@gmail.com>
2017-10-09 00:07:59 +0800

    :bug: android specific fixes

--- App.js
+++ App.js
@@ -30,6 +30,7 @@ export default class App extends React.Component {
             })
           }
         }, {
+          tabBarPosition: 'bottom',
           tabBarOptions: {
             labelStyle: { fontSize: 12 }
           }
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,16 +1,25 @@
+import {
+  Platform,
+  View
+} from 'react-native'
 import React, { Component } from 'react'
 
 import {
   Button
 } from 'react-native-elements'
 import PropTypes from 'prop-types'
-import {
-  View
-} from 'react-native'
 import { clearLikedJobs } from '../actions'
 import { connect } from 'react-redux'
 
 class SettingsScreen extends Component {
+  static navigationOptions = {
+    header: {
+      style: {
+        marginTop: Platform.OS === 'android' ? 24 : 0
+      }
+    }
+  }
+
   render () {
     return (
       <View>
