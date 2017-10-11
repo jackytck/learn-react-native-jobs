7d9f8fa8f9e127b7bc94aacc24c485e4804c0fb6
Jacky Tang <jackytck@gmail.com>
2017-10-12 01:23:09 +0800

    :sparkles: display push notification

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import {
+  Alert,
+  StyleSheet,
+  View
+} from 'react-native'
+import {
   StackNavigator,
   TabNavigator
 } from 'react-navigation'
-import { StyleSheet, View } from 'react-native'
 
 import AuthScreen from './screens/AuthScreen'
 import DeckScreen from './screens/DeckScreen'
 import MapScreen from './screens/MapScreen'
+import { Notifications } from 'expo'
 import { Provider } from 'react-redux'
 import React from 'react'
 import ReviewScreen from './screens/ReviewScreen'
 import SettingsScreen from './screens/SettingsScreen'
 import WelcomeScreen from './screens/WelcomeScreen'
+import registerForNotifications from './services/push_notifications'
 import store from './store'
 
 export default class App extends React.Component {
+  componentDidMount () {
+    registerForNotifications()
+    Notifications.addListener(notification => {
+      const { data: { text }, origin } = notification
+
+      if (origin === 'received' && text) {
+        Alert.alert(
+          'New Push Notification',
+          text,
+          [{ text: 'OK' }]
+        )
+      }
+    })
+  }
+
   render () {
     const MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
